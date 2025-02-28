46d68536324d6793947669039a6b19093a9b2b21
Kavya-gk <95424511+Kavya-gk@users.noreply.github.com>
2025-02-28 12:55:03 +0530

    Add files via upload

---
+++ ToDoListApp.java
@@ -0,0 +1,58 @@
+package ToDoListApp;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ToDoListApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\nTo-Do List Application");
+            System.out.println("1. Add Task");
+            System.out.println("2. Remove Task");
+            System.out.println("3. View Tasks");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+            
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter task: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("Task added!");
+                    break;
+                case 2:
+                    System.out.print("Enter task number to remove: ");
+                    int index = scanner.nextInt();
+                    if (index > 0 && index <= tasks.size()) {
+                        tasks.remove(index - 1);
+                        System.out.println("Task removed!");
+                    } else {
+                        System.out.println("Invalid task number!");
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nYour Tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting... Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+
+	}
+
+
